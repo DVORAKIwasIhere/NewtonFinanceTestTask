14273ad217144fc96347d410658cee016bd10a87
DVORAKIwasIhere <aleksey.altaev@mail.ru>
2022-06-08 20:21:54 +0800

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [images, setImages] = useState();
+
+  useEffect(() => {
+    fetch('https://picsum.photos/v2/list?page=2&limit=100')
+    .then(res => console.log(res))
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
